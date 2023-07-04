f04565f60a87702e99f7fb44a741638f100722d3
javiermendoza <javiermendoza@localhost>
2023-07-04 10:42:53 -0600

    if else

---
+++ 03072023/src/main/java/ejemplosJava/Ejemplo_IF.java
@@ -0,0 +1,14 @@
+package ejemplosJava;
+
+public class Ejemplo_IF {
+
+	public static void main(String[] args) {
+		int ejemplo=10;
+		if(ejemplo==0) {
+			System.out.println("Si");
+		}
+		
+
+	}
+
+}
---
+++ 03072023/src/main/java/ejemplosJava/Ejemplo_IfElse.java
@@ -0,0 +1,16 @@
+package ejemplosJava;
+
+public class Ejemplo_IfElse {
+
+	public static void main(String[] args) {
+		int edad=11;
+		if(edad>10) {
+			System.out.println("mayor que "+edad);
+		}
+		else {
+			System.out.println("Menor que "+edad);
+		}
+
+	}
+
+}
